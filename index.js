61ddab73f1bc2424c3b9f56e32e39b099779aaa7
Suprabhat kumar <suprabhat@Suprabhats-MacBook-Air.local>
2023-12-29 12:10:23 +0530

    First commit of backend application setup

---
+++ index.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('<h2> My twitter username is suprabhatkrsingh </h2>')
+})
+
+var sampleJsonContent = {
+    "glossary": {
+        "title": "example glossary",
+		"GlossDiv": {
+            "title": "S",
+			"GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+					"SortAs": "SGML",
+					"GlossTerm": "Standard Generalized Markup Language",
+					"Acronym": "SGML",
+					"Abbrev": "ISO 8879:1986",
+					"GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+						"GlossSeeAlso": ["GML", "XML"]
+                    },
+					"GlossSee": "markup"
+                }
+            }
+        }
+    }
+}
+
+app.get("/json", (req, res) => {
+    res.json(sampleJsonContent)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
